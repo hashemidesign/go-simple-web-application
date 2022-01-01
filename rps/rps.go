da006c50a9f6ade5a065c6ee842b12a62a44d3ed
Mohsen Hashemi <m.hashemi.code@gmail.com>
2022-01-01 12:21:44 +0330

    implementing rps

---
+++ rps/rps.go
@@ -0,0 +1,50 @@
+package rps
+
+import (
+	"math/rand"
+	"time"
+)
+
+const (
+	ROCK         = 0
+	PAPER        = 1
+	SCISSORS     = 2
+	PLAYERWINS   = 1
+	COMPUTERWINS = 2
+	DRAW         = 3
+)
+
+func PlayRound(playerValue int) (int, string, string) {
+	rand.Seed(time.Now().UnixNano())
+	computerValue := rand.Intn(3)
+	computerChoice := ""
+	roundResult := ""
+
+	winner := 0
+
+	switch computerValue {
+	case ROCK:
+		computerChoice = "Computer chose ROCK"
+		break
+	case PAPER:
+		computerChoice = "Computer chose PAPER"
+		break
+	case SCISSORS:
+		computerChoice = "Computer chose SCISSORS"
+		break
+	default:
+	}
+
+	if playerValue == computerValue {
+		roundResult = "It's a draw"
+		winner = DRAW
+	} else if playerValue == (computerValue+3)%3 {
+		roundResult = "Player wins"
+		winner = PLAYERWINS
+	} else {
+		roundResult = "Computer wins"
+		winner = COMPUTERWINS
+	}
+
+	return winner, computerChoice, roundResult
+}
